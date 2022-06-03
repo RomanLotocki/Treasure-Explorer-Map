c4aeef61ed3e1588dfd1f893775fb72d28141872
RomanLotocki <roman.lotocki@gmail.com>
2022-06-03 18:58:45 +0200

    back office homepage creation

---
+++ src/Controller/admin/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * This is controller managing the home page of the back office
+ * @Route("/admin/", name="app_admin_")
+ */
+class MainController extends AbstractController
+{
+    /**
+     * @Route("home", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
